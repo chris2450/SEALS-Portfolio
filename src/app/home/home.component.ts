b901543b33abdf2db84c849c848efa35e07c5950
Christian <christianbarahona2450@gmail.com>
2018-02-24 04:12:10 -0800

    third commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,25 +3,5 @@ import { FormsModule} from '@angular/forms';
 import { NgModel, NgForm} from '@angular/forms';
 import { HttpClient} from '@angular/common/http';
 
-class Form {
-  sealName: string;
-  email: string;
-  firstName: string;
-  lastName: string;
-  content: string;
-}
-
-@Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
-})
-export class HomeComponent implements OnInit {
-  public form: Form;
-
-  constructor(public http: HttpClient) {
-    this.form = new Form();
-    this.form.sealName = 'Christian Barahona';
 
 
-   }
