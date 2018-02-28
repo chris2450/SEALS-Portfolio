cedc0bd577bb5bca24cbd556b817bccd8505ce3f
Christian <christianbarahona2450@gmail.com>
2018-02-27 16:39:55 -0800

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { ContactComponent } from './contact/contact.component';
 const routes: Routes = [
 
   {
-    path: '',
+    path: 'home',
     component: HomeComponent
   },
   {
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule} from '@angular/forms';
 import { NgModel, NgForm} from '@angular/forms';
-import { HttpClient} from '@angular/common/http';
+
 
 
 
